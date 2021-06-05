9764c3b3f4e1289bf94c5a0410ed618d9a8421b7
Matthew Mahoney <mahoney.a.matthew@gmail.com>
2021-06-05 11:12:56 -0400

    Streamlined adding new posts. Added video to a post.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,20 +1,24 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({postData}) {
+export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
-       <title>{postData.title}</title>
+        <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
-    )
+  )
 }
 
 export async function getStaticPaths() {
@@ -27,7 +31,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
--- posts/first_post.md
+++ posts/first_post.md
@@ -1,11 +1,13 @@
 ---
-title: 'Two Forms of Pre-rendering'
-date: '2020-01-01'
+title: 'First Post'
+date: '2021-06-05'
 ---
 
 Next.js has two forms of pre-rendering: **Static Generation** and **Server-side Rendering**. The difference is in **when** it generates the HTML for a page.
 
 - **Static Generation** is the pre-rendering method that generates the HTML at **build time**. The pre-rendered HTML is then _reused_ on each request.
 - **Server-side Rendering** is the pre-rendering method that generates the HTML on **each request**.
-
+<div id="wrap">
+  <iframe width="100%" height="510" src="https://www.youtube.com/embed/CFXkwCtEzDQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
 Importantly, Next.js lets you **choose** which pre-rendering form to use for each page. You can create a "hybrid" Next.js app by using Static Generation for most pages and using Server-side Rendering for others.
